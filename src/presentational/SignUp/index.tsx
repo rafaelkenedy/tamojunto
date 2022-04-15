0003ad97341201bdffb9e39eb33899b136192e78
rafaelkenedy <k.rafaelalves@gmail.com>
2022-04-15 20:17:15 -0300

    SignUp update with progress bar

--- src/presentational/SignUp/index.tsx
+++ src/presentational/SignUp/index.tsx
@@ -1,97 +1,221 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import {StatusBar, View, Text} from 'react-native';
-import LoadButton from '../../components/LoadButton';
-import {
-	ScrollView,
-	TouchableHighlight,
-	TouchableOpacity,
-} from 'react-native-gesture-handler';
-import {
-	StyledView,
-	StyledStepHeader,
-	StyledCheckTitle,
-	StyledStepWrap,
-	StyledBackground,
-	StyledImage,
-	StyledText,
-	StyledInputEmail,
-	StyledLabel,
-	StyledInputPass,
-	StyledFormContainer,
-	StyledButtonContainer,
-	StyledLogosBtnContainer,
-	StyledLogo,
-	StyledFooter,
-	StyledFooterContainer,
-	StyledFooterLink,
-	StyledInputName,
-	StyledStepIcon,
-} from './styles';
+import {StatusBar} from 'react-native';
+import {ProgressSteps, ProgressStep} from '../../components/react-native-progress-steps';
+import {StyledView, StyledContainer, StyledText} from './styles';
+import TextArea from '../../components/TextArea';
+
 
 export function SignUp() {
-
 	const [isformComplete, setFormComplete] = useState(true);
+	const [email, setEmail] = useState<string>('');
+	const [userName, setUserName] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	const [checked, setChecked] = useState<boolean>(false);
 
 	return (
-		<StyledView>
+		<StyledContainer
+			source={require('../../assets/images/background_variation.png')}>
 			<StatusBar hidden />
-			<StyledBackground
-				source={require('../../assets/images/background_variation.png')}>
-				<ScrollView>
-					<StyledStepHeader>
-						<StyledStepWrap>
-							<StyledCheckTitle>{'Passo 1'}</StyledCheckTitle>
-							<StyledStepIcon
-								source={require('../../assets/icons/step_checked.png')}
-							/>
-						</StyledStepWrap>
 
-						<StyledStepWrap>
-							<StyledCheckTitle>{'Passo 2'}</StyledCheckTitle>
-							<StyledStepIcon
-								source={require('../../assets/icons/step_unchecked.png')}
-							/>
-						</StyledStepWrap>
-
-						<StyledStepWrap>
-							<StyledCheckTitle>{'Passo 3'}</StyledCheckTitle>
-							<StyledStepIcon
-								source={require('../../assets/icons/step_unchecked.png')}
-							/>
-						</StyledStepWrap>
-					</StyledStepHeader>
-					<StyledFooterContainer>
-						<StyledFooter>
+			<ProgressSteps
+				
+				progressBarColor="#827E75"
+				completedProgressBarColor="#0D4A4E"
+				activeStepNumColor="#827E75"
+				completedStepIconColor="#0D4A4E"
+				labelColor="#827E75"
+				activeStepIconBorderColor="#0D4A4E"
+				activeLabelColor="#0D4A4E"
+				labelFontSize={18}
+				disabledStepIconColor="#827E75">
+				<ProgressStep label="Passo 1" nextBtnText="Próximo">
+					<StyledView>
+						<StyledText topDistance="10px">
 							{
 								'Boas vindas! Informe os dados abaixo para fazermos seu cadastro.'
 							}
-						</StyledFooter>
-					</StyledFooterContainer>
+						</StyledText>
 
-					<StyledFormContainer>
-						<StyledLabel>{'Nome completo:'}</StyledLabel>
-						<StyledInputName />
-						<StyledLabel>{'E-mail:'}</StyledLabel>
-						<StyledInputEmail />
-						<StyledLabel>{'Defina sua senha:'}</StyledLabel>
-						<StyledInputPass />
-						<StyledLabel>{'Confirme sua senha:'}</StyledLabel>
-						<StyledInputPass />
-					</StyledFormContainer>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Nome completo:'}
+						</StyledText>
 
-					{!isformComplete&&(
-						<StyledFooterContainer>
-							<StyledFooter>{'Todos os campos são obrigatórios.'}</StyledFooter>
-						</StyledFooterContainer>
-						)
-					}
+						<TextArea
+							autoCapitalize="none"
+							keyboardType="email-address"
+							placeholder="insira nome e sobrenome"
+							value={userName}
+							onChangeText={(text) => setUserName(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'E-mail:'}
+						</StyledText>
 
-					<StyledButtonContainer>
-						<LoadButton buttonTitle="CONTINUAR >" />
-					</StyledButtonContainer>					
-				</ScrollView>
-			</StyledBackground>
-		</StyledView>
+						<TextArea
+							autoCapitalize="none"
+							keyboardType="email-address"
+							placeholder="email@email.com"
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Defina sua senha'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="defina sua senha com letras e números"
+						/>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Confirme sua senha'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="repita a senha definida acima"
+						/>
+
+						<StyledText topDistance="10px">
+							{'Todos os campos são obrigatórios.'}
+						</StyledText>
+						{/* <LoadButton buttonTitle="Continuar" /> */}
+					</StyledView>
+				</ProgressStep>
+
+				<ProgressStep
+					label="Passo 2"
+					nextBtnText="Próximo"
+					previousBtnText="Anterior">
+					<StyledView>
+						<StyledText topDistance="10px">
+							{
+								'Agora, por favor insira os dados da sua empresa (você precisa ter um CNPJ ativo).'
+							}
+						</StyledText>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Nome Fantasia'}
+						</StyledText>
+
+						<TextArea
+							placeholder="Nome que seus clientes conhecem"
+							value={userName}
+							onChangeText={(text) => setUserName(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'CNPJ'}
+						</StyledText>
+
+						<TextArea
+							placeholder="00.000.000/0000-00"
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Ramo de atuação'}
+						</StyledText>
+						<TextArea
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="Restaurante"
+						/>
+
+						
+					</StyledView>
+				</ProgressStep>
+
+				< ProgressStep
+					label="Passo 3"
+					finishBtnText="Enviar"
+					previousBtnText="Anterior">
+					<StyledView>
+						<StyledText topDistance="10px">
+							{
+								'Para te ajudar a encontrar outros empreendedores próximos, por favor informe o endereço do seu negócio.'
+							}
+						</StyledText>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'CEP'}
+						</StyledText>
+
+						<TextArea
+							autoCapitalize="none"
+							keyboardType="email-address"
+							placeholder="00000-000"
+							value={userName}
+							onChangeText={(text) => setUserName(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Endereço:'}
+						</StyledText>
+
+						<TextArea
+							autoCapitalize="none"
+							keyboardType="email-address"
+							placeholder="Rua Fulana de Tal"
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+							blurOnSubmit={false}
+						/>
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Número'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="XX"
+						/>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Complemento'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="Loja XX"
+						/>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Estado'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="Estado"
+						/>
+
+						<StyledText textSize="16px" textWeight="bold" topDistance="24px">
+							{'Município'}
+						</StyledText>
+						<TextArea
+							autoCapitalize="none"
+							secureTextEntry
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							placeholder="Município"
+						/>
+					</StyledView>
+				</ProgressStep>
+			</ProgressSteps>
+		</StyledContainer>
 	);
 }
--- src/presentational/SignUp/styles.ts
+++ src/presentational/SignUp/styles.ts
@@ -1,32 +1,17 @@
 import {TextProps} from 'react-native';
 import styled from 'styled-components/native';
 import theme from '../../styles/theme';
-import TextArea from '../../components/TextArea';
-import { TouchableOpacityProps } from 'react-native';
 
 const elevationValue = 3;
 interface TextType extends TextProps {
 	textWeight?: string;
-	textColor?: string;
 	textSize?: string;
-}
-
-interface ButtonProps extends TouchableOpacityProps{
-	isDisabled?: boolean;
-	color?: string;
-	children?: string;	
+	topDistance?: string;
 }
 
 export const StyledView = styled.View`
 	flex: 1;
-`;
-
-export const StyledStepHeader = styled.View`
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-	height: 50px;	
-	margin: 40px;
+	margin-horizontal: 24px;
 `;
 
 export const StyledStepWrap = styled.View`
@@ -36,131 +21,20 @@ export const StyledStepWrap = styled.View`
 	justify-content: flex-end;
 `;
 
-export const StyledCheckTitle = styled.Text<TextType>`
-	
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '13px'};
-	font-weight: ${(props) => props.textWeight || 400};
-	width: 48px;
-`;
-
-export const StyledStepIcon = styled.Image`	
-	height: 24px;
-	width: 24px;
-	
-	
-	
-`;
-
-
-export const StyledBackground = styled.ImageBackground`
+export const StyledContainer = styled.ImageBackground`
     flex: 1;
     resizeMethod="resize";
-    justify-content: flex-end;
-	
-`;
-
-export const StyledFormContainer = styled.View`
-	margin-horizontal: 24px;
-`;
-
-export const StyledInputName = styled(TextArea).attrs({
-	placeholder: 'Insira seu nome e sobrenome...',
-	borderColor: theme.colors.border_gray,
-	borderSize: '1px',
-	shadowBox: elevationValue,
-})`
-	align-self: center;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '20px'};
-	font-weight: ${(props) => props.textWeight || 400};
-`;
-
-export const StyledInputEmail = styled(TextArea).attrs({
-	placeholder: 'Digite seu email...',
-	borderColor: theme.colors.border_gray,
-	borderSize: '1px',
-	shadowBox: elevationValue,
-})`
-	align-self: center;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '20px'};
-	font-weight: ${(props) => props.textWeight || 400};
-`;
-
-export const StyledLabel = styled.Text`
-	font-size: 16px;
-	color: ${() => theme.colors.black_pearl};
-	font-weight: 700;
-	margin-top: 24px;
-	margin-bottom: 12px;
-`;
-
-export const StyledInputPass = styled(TextArea).attrs({
-	borderColor: theme.colors.border_gray,
-	borderSize: '1px',
-	placeholder: 'Digite sua senha...',
-	shadowBox: elevationValue,
-})`
-	align-self: center;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '20px'};
-	font-weight: ${(props) => props.textWeight || 400};
-`;
-
-export const StyledButtonContainer = styled.View`
-	margin-horizontal: 34px;
-	//background-color: ${theme.colors.athens_gray};
-
-	//background-color: red;
-`;
-
-export const StyledLogosBtnContainer = styled.View`
-	margin: 24px;
-	padding-horizontal: 64px;
-	justify-content: space-between;
-	flex-direction: row;
-`;
-
-export const StyledFooterContainer = styled.View`
-	margin-horizontal: 24px;
-	margin-bottom: 41px;
-`;
-
-export const StyledLogo = styled.Image<ButtonProps>`
-	
-  
-	width: 48px;
-	height: 48px;
-`;
-
-export const StyledImage = styled.Image`
-	width: 120px;
-	height: 146.3px;
-	align-self: center;
-	margin-top: 78px;
+    justify-content: flex-end;	
 `;
 
 export const StyledText = styled.Text<TextType>`
-	align-self: center;
-	margin-top: 20px;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
+	margin-top: ${(props) => props.topDistance || '1px'};
+	color: ${theme.colors.black_pearl};
 	font-size: ${(props) => props.textSize || '20px'};
 	font-weight: ${(props) => props.textWeight || 400};
 `;
 
-export const StyledFooter = styled.Text<TextType>`
-	margin-top: 20px;
-	margin-right: 24px;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '20px'};
-	font-weight: ${(props) => props.textWeight || 400};
-`;
 
-export const StyledFooterLink = styled.Text<TextType>`
-	margin-top: 12px;
-	margin-right: 24px;
-	color: ${(props) => props.textColor || theme.colors.black_pearl};
-	font-size: ${(props) => props.textSize || '20px'};
-	font-weight: ${(props) => props.textWeight || 700};
-`;
+export const StyledButton = styled.TouchableOpacity.attrs(() => ({
+    activeOpacity: 0.5,
+}))(() => '');
